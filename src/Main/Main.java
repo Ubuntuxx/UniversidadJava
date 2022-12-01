600d4aa5e90655d6fda6fef22b15ec010038202b
Jose Luis <unixluisx@protonmail.ch>
2022-12-01 02:20:00 -0600

    cambios

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -4,6 +4,10 @@ import Operaciones.Aritmetica;
 
 public class Main {
     public static void main(String[] args) {
+
+        //variables locales
+        int a = 10;
+        int b = 12;
         Aritmetica aritmetica = new Aritmetica();
         System.out.println(aritmetica.a);
         System.out.println(aritmetica.b);
