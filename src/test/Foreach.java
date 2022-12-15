fc0a97690857d84035af3c6ac18a6ad965ce6bb4
Jose Luis <unixluisx@protonmail.ch>
2022-12-15 17:49:57 -0600

    Seccion27 - ForEach

---
+++ src/test/Foreach.java
@@ -0,0 +1,11 @@
+package test;
+
+public class Foreach {
+    int edadaes[] = {123,4,5,6,7,8,4};
+    
+    public void recorerr(int edadaes[]){
+        for (int variableRecolectora:edadaes) {
+            System.out.println(edadaes);
+        }
+    }
+}
