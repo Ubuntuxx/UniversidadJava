c74267bab24b644337423c0ef615c8ad3bd1cbfe
Jose Luis <unixluisx@protonmail.ch>
2022-12-16 00:53:23 -0600

    Seccion29 - Modificadores de tipo public

---
+++ src/Main/Main.java
@@ -0,0 +1,11 @@
+package Main;
+
+import paquete1.ModificadoresAcceso;
+
+public class Main {
+    public static void main(String[] args) {
+        ModificadoresAcceso modificadoresAcceso = new ModificadoresAcceso();
+        System.out.println("modificadoresAcceso = " + modificadoresAcceso);
+        System.out.println(modificadoresAcceso.atributo);
+    }
+}
---
+++ src/paquete1/ModificadoresAcceso.java
@@ -0,0 +1,13 @@
+package paquete1;
+
+public class ModificadoresAcceso {
+    public String atributo = "Atributo publico";
+
+    public ModificadoresAcceso(){
+        System.out.println(" Constructor vacio publico" );
+    }
+
+    public void metodoPublicov(){
+        System.out.println("Metodo publico");
+    }
+}
