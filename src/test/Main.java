472d0057340717c47de68f425e093157492b3d03
Jose Luis <unixluisx@protonmail.ch>
2022-12-20 01:37:16 -0600

    Seccion 31 - Polimorfismo

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,10 +1,21 @@
 package test;
 
+import domain.Empleado;
 import domain.Gerente;
 
 public class Main {
     public static void main(String[] args) {
-        Gerente gerente = new Gerente("Luis",1000,"Sistemas");
-        System.out.println(gerente.obtenerDetalles());
+        Empleado empleado = new Empleado("Juan", 5000);
+        //System.out.println("empleado.obtenerDetalles() = " + empleado.obtenerDetalles());
+        imprimir(empleado);
+
+        Gerente gerente = new Gerente("Karla", 1999, "Contabilidad");
+        //System.out.println("gerente.obtenerDetalles() = " + gerente.obtenerDetalles());
+
+        imprimir(gerente);
+    }
+
+    public static void imprimir(Empleado em) {
+        System.out.println("empleado = " + em.obtenerDetalles());
     }
 }
