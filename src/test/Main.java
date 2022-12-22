8ee208695f2eaefd5df528a7ee73aeba4273e92f
Jose Luis <unixluisx@protonmail.ch>
2022-12-21 23:45:21 -0600

    Borrado de comentarios

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,11 +6,9 @@ import domain.Gerente;
 public class Main {
     public static void main(String[] args) {
         Empleado empleado = new Empleado("Juan", 5000);
-        //System.out.println("empleado.obtenerDetalles() = " + empleado.obtenerDetalles());
         imprimir(empleado);
 
         Gerente gerente = new Gerente("Karla", 1999, "Contabilidad");
-        //System.out.println("gerente.obtenerDetalles() = " + gerente.obtenerDetalles());
 
         imprimir(gerente);
     }
