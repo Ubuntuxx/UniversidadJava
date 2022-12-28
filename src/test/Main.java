0fb73a98eef626e5f6f105750cda5509b97d3ed7
Jose Luis <unixluisx@protonmail.ch>
2022-12-27 23:59:39 -0600

    Seccion 31 - Instance of

--- src/test/Main.java
+++ src/test/Main.java
@@ -6,14 +6,19 @@ import domain.Gerente;
 public class Main {
     public static void main(String[] args) {
         Empleado empleado = new Empleado("Juan", 5000);
-        imprimir(empleado);
-
         Gerente gerente = new Gerente("Karla", 1999, "Contabilidad");
-
-        imprimir(gerente);
+        Determinar(empleado);
     }
 
-    public static void imprimir(Empleado em) {
-        System.out.println("empleado = " + em.obtenerDetalles());
+    public static void Determinar(Empleado em) {
+        if (em instanceof Gerente) {
+            System.out.println("Es de Tipo gerente");
+        }
+        else if (em instanceof Empleado) {
+            System.out.println("Es de tipo gerente");
+        }
+        else if (em instanceof Object) {
+            System.out.println("Es de tipo Object");
+        }
     }
 }
