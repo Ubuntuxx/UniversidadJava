c00a52a8d2c8231a8bb9626b412f96aaa0b24e98
Jose Luis <unixluisx@protonmail.ch>
2023-01-02 23:00:18 -0600

    API Colecciones

--- src/genericos/ClaseGenerica.java
+++ src/genericos/ClaseGenerica.java
@@ -1,4 +1,13 @@
 package genericos;
 
-public class ClaseGenerica {
+public class ClaseGenerica<T> {
+    private T objetc;
+
+    public ClaseGenerica(T objeto) {
+        this.objetc = objeto;
+    }
+
+    public void obtenerTipo() {
+        System.out.println("El tipo es: " + objetc.getClass().getSimpleName());
+    }
 }
--- src/test/Main.java
+++ src/test/Main.java
@@ -1,50 +1,16 @@
 package test;
 
+import genericos.ClaseGenerica;
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        List miList = new ArrayList();
-        miList.add("Lunes");
-        miList.add("Martes");
-        miList.add("Miercoles");
-        miList.add("Jueves");
-        miList.add("Viernes");
-        miList.add("Sabado");
+        ClaseGenerica<Integer> objetoInt = new ClaseGenerica<Integer>(23);
+        objetoInt.obtenerTipo();
 
-        Set miSet = new HashSet();
-        miSet.add("Lunes");
-        miSet.add("Martes");
-        miSet.add("Miercoles");
-        miSet.add("Jueves");
-        miSet.add("Viernes");
-        miSet.add("Sabado");
 
-//        imprime(miSet);
-//        System.out.println();
-//        imprime(miList);
-
-        Map miMap = new HashMap();
-
-        //Modelo llave valor
-        //         llave      valor
-        miMap.put("Valor1", "Juan");
-        miMap.put("Valor2", "Carlos");
-        miMap.put("Valor3", "Roasario");
-
-        //                              LLAVE
-        System.out.println(miMap.get("Valor1"));
-
-        //para obtener todas la llaves (Solo las llaves)
-        imprime(miMap.keySet());
-
-        //para imprimir valores (Solo los valores)
-        imprime(miMap.values());
-    }
-
-    public static void imprime(Collection coleccion) {
-        coleccion.forEach(elemento -> {
-            System.out.println(elemento);
-        });
+        ClaseGenerica<String> objetoString = new ClaseGenerica<String>("Luia");
+        objetoString.obtenerTipo();
     }
 }
